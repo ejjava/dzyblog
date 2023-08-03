6a42f8e6fd354e05367f7a2bb183c14d696335bb
root <root@localhost>
2023-08-03 17:51:56 +0800

    2023-08-03 17:51:54

--- 2023/8.2/SplicingArticle.js
+++ 2023/8.2/SplicingArticle.js
@@ -2,26 +2,22 @@
 var contentDiv = document.getElementById("content");
 
 // 添加更多的HTML内容
-var cardHtml = `<div class="card m-2 mdui-ripple">
+var cardHtml = `<div class="card m-2">
         <div class="card-body">
             <blockquote class="blockquote">
-                <h4 class="article-title"><a class="text-body" href="/2023/8.2/index.html"><span>八月第一天的夜晚</span></a></h4>
-                <footer class="blockquote-footer">2023.8.2</cite></footer>
-                <a href="" class="badge badge-info">生活</a>
-            </blockquote>
-            <h6 class="article-title text-body"><a class="text-body" href="/2023/8.2/index.html"><span>这个八月第一天晚上随手拍的一张照片，就简单拿来分享一下</span></a></h6>
+                <p class="mb-0">八月第一天的晚上</p>
+                <footer class="blockquote-footer">DZY</cite></footer>
+            </blockquote>           
+<a href="https://s1.ax1x.com/2023/08/02/pPPIEqO.jpg" class="glightbox">
+  <img class="img-fluid" src="https://s1.ax1x.com/2023/08/02/pPPIEqO.jpg" alt="image" />
+</a>
+    
+            <cite title="Source Title">
+            
+          <br>  <br>8.1晚间的一张图片，随便分享一下了（狗头）
         </div>
     </div>
-    <div class="card m-2 mdui-ripple">
-        <div class="card-body">
-            <blockquote class="blockquote">
-                <h4 class="article-title"><a class="text-body" href="/2023/8.1/index.html"><span>Hello World</span></a></h4>
-                <footer class="blockquote-footer">2023.8.1</cite></footer>
-                <a href="" class="badge badge-info">网站历程</a>
-            </blockquote>
-            <h6 class="article-title text-body"><a class="text-body" href="/2023/8.1/index.html"><span>这是我手搓的一个乐射博客</span></a></h6>
-        </div>
-    </div>`;
+    `;
 
 // 将生成的HTML内容添加到div元素中
 contentDiv.innerHTML = cardHtml;
@@ -33,13 +29,13 @@ var contentDiv = document.getElementById("Footer");
 // 添加更多的HTML内容
 var cardHtml = `
 
-
-
+ <div id="Footer"></div>
     <div class="container-fluid">
         <h6 class="text-center">&copy;2023DZY's Blog</h6>
         <p class="mb-0 text-center"><a class="nav-link" href="https://getbootstrap.com/">框架:Bootstrap 主题:自己写的^0^</a></p>
 
     </div>
+    
     
     
     `;
@@ -54,9 +50,7 @@ contentDiv.innerHTML = cardHtml;
 var contentDiv = document.getElementById("head");
 
 // 添加更多的HTML内容
-var cardHtml = `
-
- <ul class="nav">
+var cardHtml = `<ul class="nav">
         <li class="nav-item">
             <a class="nav-link disabled">DZY's Blog</a>
         </li>
@@ -71,7 +65,30 @@ var cardHtml = `
             <a class="nav-link" href="./About.html">关于</a>
         </li>
     </ul>
-    <br>
+    <br> 
+    
+    `;
+
+// 将生成的HTML内容添加到div元素中
+contentDiv.innerHTML = cardHtml;
+
+
+// 获取div元素
+var contentDiv = document.getElementById("Details");
+
+// 添加更多的HTML内容
+var cardHtml = ` <div class="card m-2">
+  <div class="card-body">
+    <h6>发布时间:2023.8.2</h6>
+  
+            </script>本站总访问量<span id="busuanzi_value_site_pv"></span>次！
+  </div>
+</div>
+    
+    <div class="alert alert-info m-2" role="alert">
+        发表违反中华人民共和国法律,不良广告，辱骂等内容，后果自负！
+    </div>
+    
     `;
 
 // 将生成的HTML内容添加到div元素中
