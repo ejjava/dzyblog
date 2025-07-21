ce1c8bb897be585bcc26b5a2dd077911889cc16d
root <root@localhost>
2025-07-21 15:17:40 +0800

    2025-07-21 15:17:38

--- archives/js/sou.js
+++ archives/js/sou.js
@@ -1,18 +1,32 @@
+// 搜索功能
+function myFunction() {
+    try {
+        const input = document.getElementById('myInput');
+        const filter = input.value.toUpperCase();
+        const ul = document.getElementById('myUL');
+        const li = ul.getElementsByTagName('li');
 
-  function myFunction() {
-    var input, filter, ul, li, a, i, txtValue;
-    input = document.getElementById('myInput');
-    filter = input.value.toUpperCase();
-    ul = document.getElementById("myUL");
-    li = ul.getElementsByTagName('li');
-  
-    for (i = 0; i < li.length; i++) {
-      a = li[i].getElementsByTagName("a")[0];
-      txtValue = a.textContent || a.innerText;
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
-        li[i].style.display = "";
-      } else {
-        li[i].style.display = "none";
-      }
+        for (let i = 0; i < li.length; i++) {
+            const a = li[i].getElementsByTagName('a')[0];
+            if (a) {
+                const txtValue = a.textContent || a.innerText;
+                if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                    li[i].style.display = "";
+                } else {
+                    li[i].style.display = "none";
+                }
+            }
+        }
+    } catch (error) {
+        console.error('搜索出错:', error);
     }
-  }
+}
+
+// 初始化搜索功能
+document.addEventListener('DOMContentLoaded', function() {
+    const input = document.getElementById('myInput');
+    if (input) {
+        input.addEventListener('keyup', myFunction);
+        console.log('基础搜索功能已初始化');
+    }
+});
